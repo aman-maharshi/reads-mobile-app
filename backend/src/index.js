a52937291d919507b992db2c527d8fc61abc4b31
aman-maharshi <amanm4harshi@gmail.com>
2025-03-16 19:02:35 +0530

    book schema added

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import "dotenv/config"
 import authRoutes from './routes/authRoutes.js'
+import bookRoutes from './routes/bookRoutes.js'
 import { connectDB } from './lib/db.js'
 
 const app = express()
@@ -11,6 +12,7 @@ app.use(express.json())
 
 // Routes
 app.use('/api/auth', authRoutes)
+app.use('/api/books', bookRoutes)
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
