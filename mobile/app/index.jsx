b82a8248a8d5799ba5a4d7fed010c316ec0434cb
aman-maharshi <amanm4harshi@gmail.com>
2025-03-17 23:57:58 +0530

    styles and fonts init

--- mobile/app/index.jsx
+++ mobile/app/index.jsx
@@ -1,15 +1,20 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
+import { Image } from "expo-image"
+import styles from "@/styles/create.styles"
 
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style={styles.container}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <View style={styles.card}>
+
+        <View style={styles.header}>
+          <Text style={styles.title}>Create a new post</Text>
+          <Text style={styles.subtitle}>Fill in the form below to create a new post</Text>
+        </View>
+      </View>
     </View>
-  );
+  )
 }
+
