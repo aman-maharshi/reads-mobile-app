cc2002c2da7cd48d4f23f0370fcba4d045d06f61
aman-maharshi <amanm4harshi@gmail.com>
2025-03-21 23:32:02 +0530

    root UI minor change

--- mobile/app/index.jsx
+++ mobile/app/index.jsx
@@ -1,4 +1,4 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
 import { Image } from "expo-image"
 import styles from "@/styles/create.styles"
 import { Link } from "expo-router";
@@ -8,7 +8,16 @@ export default function Index() {
     <View
       style={styles.container}
     >
-      <Link href="/(auth)">Login</Link>
+      <Link href="/(auth)" asChild>
+        <TouchableOpacity style={{ marginTop: 40, padding: 10, borderRadius: 10 }}>
+          <Text style={{fontWeight: "bold", textAlign: "center", fontSize: 16}}>Login</Text>
+        </TouchableOpacity>
+      </Link>
+      <Link href="/(auth)/signup" asChild>
+        <TouchableOpacity style={{ marginTop: 40 }}>
+        <Text style={{fontWeight: "bold", textAlign: "center", fontSize: 16}}>Sign Up</Text>
+        </TouchableOpacity>
+      </Link>
     </View>
   )
 }
