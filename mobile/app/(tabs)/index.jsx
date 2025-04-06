6f0c8cdb0c3b502ddb68df526036f968192e7fe1
aman-maharshi <amanm4harshi@gmail.com>
2025-04-06 18:04:16 +0530

    home fetch books updates

--- mobile/app/(tabs)/index.jsx
+++ mobile/app/(tabs)/index.jsx
@@ -1,7 +1,10 @@
-import { View, Text, TouchableOpacity, Image, FlatList } from 'react-native'
-import React from 'react'
+import { useState, useEffect } from 'react'
+import { View, Text, TouchableOpacity, FlatList, ActivityIndicator } from 'react-native'
 import { useAuthStore } from "../../store/authStore"
 import styles from "../../styles/home.styles"
+import { BASE_URL } from '../../constants/apiUrl'
+import { Image } from 'expo-image'
+import { Ionicons } from '@expo/vector-icons'
 
 const Home = () => {
   const { user, token, logout } = useAuthStore()
@@ -10,6 +13,47 @@ const Home = () => {
   const [refreshing, setRefreshing] = useState(false)
   const [page, setPage] = useState(1)
   const [hasMore, setHasMore] = useState(true)
+
+  const fetchBooks = async (pageNum = 1, refresh = false) => {
+    try {
+      if (refresh) {
+        setRefreshing(true)
+      } else if (pageNum === 1) {
+        setLoading(true)
+      }
+
+      const response = await fetch(`${BASE_URL}/api/books/all?page=${pageNum}&limit=2`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      const data = await response.json()
+
+      if (!response.ok) {
+        throw new Error(data.error || "Failed to fetch books")
+      }
+
+      setBooks((prevBooks) => [...prevBooks, ...data.books])
+      setHasMore(pageNum < data.totalPages)
+      setPage(pageNum)
+
+    } catch (error) {
+      console.error("Error fetching books:", error)
+    } finally {
+      if (refresh) {
+        setRefreshing(false)
+      } else {
+        setLoading(false)
+      }
+    }
+  }
+
+  useEffect(() => {
+    fetchBooks()
+  }, [])
+
 
   const bookCard = ({ item }) => (
     <View style={styles.bookCard}>
@@ -35,7 +79,8 @@ const Home = () => {
 
   return (
     <View style={styles.container}>
-      <FlatList 
+      {loading && <ActivityIndicator color="black" />}
+      <FlatList
         data={books}
         renderItem={bookCard}
         keyExtractor={(item) => item.id}
